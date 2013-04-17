f599a3ae50267869dacff1efb77dfb9fdf5e51eb
Glen Swift <glen.swift@gmail.com>
2013-04-18 01:42:01 +0300

    getData() renamed to downloadData(). Added createDatabase() which passes all .xls files and create df.csv file with all data. Added getData() which returns dataframe containing data about selected city(it seems like it makes mistake in some cases at the moment)

--- init.R
+++ init.R
@@ -1,8 +1,8 @@
-getData <- function() {
+downloadData <- function() {
   library(RCurl)
   library(XML)
   #Удалить файлы в директории ./data/
-  #do.call(file.remove,list(list.files(paste0(getwd(),'/data', collapse = ''), full.names=TRUE)))
+  do.call(file.remove,list(list.files(paste0(getwd(),'/data', collapse = ''), full.names=TRUE)))
   #Соединится с сервером и спарсить последнюю страницу в паганации
   url <- 'http://dom.ria.ua/ru/search/?advert_type=1&realty_type=&category=0&language=ru&limit=100&page=1'
   cookie <- 'Real Estate Data Parsing'
@@ -11,9 +11,31 @@ getData <- function() {
   pagination <- xpathSApply(dom, "//div[@class='page']", xmlValue)
   maxPagination <- as.numeric(strsplit(pagination, split='...', fixed = TRUE)[[1]][2])
   #Скачать файлы в директорию ./data/
-  for (i in 1534:maxPagination) {
+  for (i in 1:maxPagination) {
     url <- paste0('http://dom.ria.ua/ru/exportsearch/?advert_type=1&realty_type=&category=0&language=ru&limit=100&page=',i,'&xls=1', collapse = '')
     dir <- paste0(getwd(),'/data/', i, '.xls', collapse = '')
     f <- download.file(url, dir, mode='wb')
   }
+}
+
+createDatabase <- function() {
+  library(RODBC)
+  files <- length(list.files(paste0(getwd(),'/data', collapse = ''), full.names=TRUE))
+  df <- data.frame()
+  for (i in 1:files) {
+    channel <- odbcConnectExcel(paste0(getwd(),'/data/1.xls', collapse = ''))
+    sqlTables(channel)
+    sh1 <- sqlFetch(channel, "Page 1")
+    df <- rbind(data.frame(sh1), df)
+    odbcClose(channel)
+  }
+  write.csv(df, file=paste0(getwd(),'/csvdata/database.csv'), row.names = FALSE)
+}
+
+getData <- function(city) {
+  data <- data.frame(read.csv(paste0(getwd(),'/csvdata/database.csv')))
+  if (!missing(city)) {
+    data <- subset(data, Город == city)
+  }
+  return(data)
 }
